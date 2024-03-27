dee3e0569ec8df4c86bfb57a5bd1722328d09c29
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-27 08:05:09 -0300

    fast commit

--- src/components/ItemsContainer/ItemsContainer.tsx
+++ src/components/ItemsContainer/ItemsContainer.tsx
@@ -2,8 +2,10 @@ import { ItemCard } from '@/components/ItemCard/ItemCard'
 
 export function ItemsContainer() {
   return (
-    <div className="ml-10 mt-10">
-      <h2 className="border w-fit border-b-gray-400 border-r-0 border-l-0 border-t-0 mb-10">Nossos projetos{'   '}</h2>
+    <div className=" mt-10">
+      <h2 className="ml-10 border w-fit border-b-gray-400 border-r-0 border-l-0 border-t-0 mb-10">
+        Nossos projetos{'   '}
+      </h2>
       <div className="p-10 flex flex-wrap justify-center md:justify-start">
         <ItemCard />
       </div>

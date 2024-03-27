0818565287b8d21c070a11b338de108f0bfc7f57
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-27 18:11:08 -0300

    fast commit

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,8 +7,53 @@ export async function POST(req: Request) {
   const { action, id } = body
   switch (action) {
     case 'getProductsAvailable':
-      return await getProductsAvailable()
+      try {
+        const productsAvailable = await getProductsAvailable()
+        return new Response(JSON.stringify(productsAvailable), {
+          status: 200, // OK
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      } catch (error) {
+        return new Response(JSON.stringify({ message: 'Failed to get products available.' }), {
+          status: 500, // Internal Server Error
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      }
     case 'getProductById':
-      return await getProductById(id)
+      try {
+        const product = await getProductById(id)
+        if (!product) {
+          return new Response(JSON.stringify({ message: 'Product not found.' }), {
+            status: 404, // Not Found
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+        }
+        return new Response(JSON.stringify(product), {
+          status: 200, // OK
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      } catch (error) {
+        return new Response(JSON.stringify({ message: 'Failed to get product by id.' }), {
+          status: 500, // Internal Server Error
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+      }
+    default:
+      return new Response(JSON.stringify({ message: 'Invalid action.' }), {
+        status: 400, // Bad Request
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
   }
 }

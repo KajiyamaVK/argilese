ab61a45b8bb104d3061403b7fe88b050b95a1d3a
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-27 20:25:10 -0300

    fast commit

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -7,53 +7,8 @@ export async function POST(req: Request) {
   const { action, id } = body
   switch (action) {
     case 'getProductsAvailable':
-      try {
-        const productsAvailable = await getProductsAvailable()
-        return new Response(JSON.stringify(productsAvailable), {
-          status: 200, // OK
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-      } catch (error) {
-        return new Response(JSON.stringify({ message: 'Failed to get products available.' }), {
-          status: 500, // Internal Server Error
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-      }
+      return await getProductsAvailable()
     case 'getProductById':
-      try {
-        const product = await getProductById(id)
-        if (!product) {
-          return new Response(JSON.stringify({ message: 'Product not found.' }), {
-            status: 404, // Not Found
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          })
-        }
-        return new Response(JSON.stringify(product), {
-          status: 200, // OK
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-      } catch (error) {
-        return new Response(JSON.stringify({ message: 'Failed to get product by id.' }), {
-          status: 500, // Internal Server Error
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-      }
-    default:
-      return new Response(JSON.stringify({ message: 'Invalid action.' }), {
-        status: 400, // Bad Request
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
+      return await getProductById(id)
   }
 }
--- src/components/ItemsContainer/ItemsContainer.tsx
+++ src/components/ItemsContainer/ItemsContainer.tsx
@@ -8,7 +8,7 @@ interface IProductShow {
   productImages: string
 }
 
-let products: IProductShow[]
+let products: IProductShow[] = []
 export async function ItemsContainer() {
   async function getAllProducts() {
     await fetch(`${process.env.API_URL}/api/products`, {
@@ -23,27 +23,28 @@ export async function ItemsContainer() {
   }
 
   await getAllProducts()
-
+  console.log('products', products)
   return (
     <div className=" mt-10 ">
       <h2 className="ml-10 border w-fit border-b-gray-400 border-r-0 border-l-0 border-t-0 mb-10">
         Nossos projetos{'   '}
       </h2>
       <div className="p-20 flex flex-wrap justify-center md:justify-start">
-        {products.map((product) => {
-          const productImages = product.productImages.split(';')
+        {products.length > 0 &&
+          products.map((product) => {
+            const productImages = product.productImages.split(';')
 
-          return (
-            <ItemCard
-              key={product.id}
-              id={product.id}
-              description={product.productDescription}
-              itemName={product.productName}
-              itemUrl={productImages[0]}
-              price={product.price}
-            />
-          )
-        })}
+            return (
+              <ItemCard
+                key={product.id}
+                id={product.id}
+                description={product.productDescription}
+                itemName={product.productName}
+                itemUrl={productImages[0]}
+                price={product.price}
+              />
+            )
+          })}
       </div>
     </div>
   )

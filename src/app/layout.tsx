f665cef0360f6024dc4712c19d3b2c4c6c15073b
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-29 09:35:46 -0300

    fast commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import './globals.css'
-import { Topbar } from '@/components/Topbar/Topbar'
+// import { Topbar } from '@/components/Topbar/Topbar'
 
 const roboto = Roboto({ weight: '400', subsets: ['latin'] })
 

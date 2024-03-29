37c460e60866f77f394243d8f3c6846320a39a28
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-29 09:34:57 -0300

    fast commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           className="absolute flex flex-wrap justify-center w-full bg-center bg-cover -z-10 top-0 h-full"
           style={{ backgroundImage: 'url(/bannerBG.png)', filter: 'blur(8px)' }}
         />
-        <Topbar />
+        {/* <Topbar /> */}
         {children}
       </body>
     </html>

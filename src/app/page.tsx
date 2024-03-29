1489a72a8e51b823f2db8808ae32d912629fd68d
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-03-29 10:26:08 -0300

    fast commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 //import { ItemsContainer } from '@/components/ItemsContainer/ItemsContainer'
-//import { MainBanner } from '@/components/MainBanner/MainBanner'
+import { MainBanner } from '@/components/MainBanner/MainBanner'
 
 export default function Home() {
   return (
     <div>
-      teste
-      {/* <MainBanner />
-      <ItemsContainer /> */}
+      <MainBanner />
+      {/* <ItemsContainer /> */}
     </div>
   )
 }

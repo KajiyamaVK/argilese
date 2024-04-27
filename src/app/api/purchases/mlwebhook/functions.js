8e6089d5a1127aeaed316a73f122cab52b2bf935
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-27 14:28:58 -0300

    fast commit

--- src/app/api/purchases/mlwebhook/functions.js
+++ src/app/api/purchases/mlwebhook/functions.js
@@ -1,9 +1,9 @@
 import { Payment } from '@mercadopago/sdk-react'
-import MercadoPagoConfig from 'mercadopago'
+import mercadopago from 'mercadopago'
 
 // paymentId is a string. PLEASE DO NOT HAVE A GREAT IDEA TO CHANGE IT TO NUMBER
 export async function getPaymentData(paymentId) {
-  const client = new MercadoPagoConfig({ accessToken: process.env.MERCADO_PAGO_TOKEN })
+  const client = new mercadopago({ accessToken: process.env.MERCADO_PAGO_TOKEN })
   const payment = new Payment(client)
 
   await payment

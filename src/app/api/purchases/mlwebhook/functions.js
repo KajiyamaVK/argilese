30cce5829e30827d6219cf5d27872e0c41ad38af
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-27 14:44:21 -0300

    fast commit

--- src/app/api/purchases/mlwebhook/functions.js
+++ src/app/api/purchases/mlwebhook/functions.js
@@ -1,17 +1,17 @@
-import { Payment } from '@mercadopago/sdk-react'
-import mercadopago from 'mercadopago'
-
 // paymentId is a string. PLEASE DO NOT HAVE A GREAT IDEA TO CHANGE IT TO NUMBER
 export async function getPaymentData(paymentId) {
-  console.log('1')
-  const client = new mercadopago({ accessToken: process.env.MERCADO_PAGO_TOKEN })
-  console.log('2')
-  const payment = new Payment(client)
-  console.log('3')
-  await payment
-    .get({
-      id: paymentId,
-    })
-    .then(console.log)
-    .catch(console.log)
+  const data = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.MERCADO_PAGO_TOKEN}`,
+    },
+  }).then((response) => {
+    console.log('response.status', response.status)
+    if (response.status === 200) {
+      return response.json()
+    }
+    throw new Error('Failed to fetch payment data')
+  })
+
+  console.log('data', data)
 }

7adf35fe3e00edfab6bca25be72af9ed5e422528
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-29 05:39:34 -0300

    fast commit

--- src/app/api/purchases/mlwebhook/functions.js
+++ src/app/api/purchases/mlwebhook/functions.js
@@ -1,15 +1,18 @@
 // paymentId is a string. PLEASE DO NOT HAVE A GREAT IDEA TO CHANGE IT TO NUMBER
 export async function getPaymentData(paymentId) {
-  await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+  const payment = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
     method: 'GET',
     headers: {
       Authorization: `Bearer ${process.env.MERCADO_PAGO_TOKEN}`,
     },
   }).then((response) => {
     if (response.status === 200) {
-      console.log('123123')
+      console.log('123123', response)
       return response.json()
+      //return response
     }
     throw new Error('Failed to fetch payment data')
   })
+
+  return payment
 }

9ce7ed6357f889c441484b68c3b95d2b70e3faa2
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-29 06:30:41 -0300

    fast commit

---
+++ src/app/api/purchases/mlwebhook/functions.ts
@@ -0,0 +1,68 @@
+import { getDatabaseConnection } from '@/utils/databaseFunctions/createConnection'
+import { FieldPacket, QueryResult } from 'mysql2'
+
+// paymentId is a string. PLEASE DO NOT HAVE A GREAT IDEA TO CHANGE IT TO NUMBER
+export async function getPaymentData(paymentId: string) {
+  const payment = await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${process.env.MERCADO_PAGO_TOKEN}`,
+    },
+  }).then((response) => {
+    if (response.status === 200) {
+      return response.json()
+    }
+    throw new Error('Failed to fetch payment data')
+  })
+
+  return payment
+}
+
+export async function updatePaymentStatus({ paymentId, status }: { paymentId: string; status: string }) {
+  const Conn = await getDatabaseConnection()
+
+  const updateQuery = `
+    UPDATE purchasePayments
+    SET status = ?
+    WHERE paymentId = ?
+  `
+
+  await Conn.query(updateQuery, [status, paymentId])
+    .catch((err) => {
+      console.error('Failed to update payment status', err)
+    })
+    .finally(() => {
+      Conn.end()
+    })
+}
+
+interface IGetCustomerDataResponse {
+  customerEmail: string
+  purchaseIdFK: number
+  customerName: string
+}
+export async function getCustomerData(paymentId: number) {
+  const Conn = await getDatabaseConnection()
+
+  const query = `
+    SELECT pd.customerEmail, pp.purchaseIdFK, pd.customerName
+    FROM purchaseDeliveries pd 
+    JOIN purchasePayments pp  
+      ON pd.purchaseIdFK = pd.purchaseIdFK = pp.purchaseIdFK;
+    WHERE pp.id = ?
+  `
+
+  const customerData: IGetCustomerDataResponse[] = await Conn.query(query, [paymentId])
+    .then(([results]: [QueryResult, FieldPacket[]]) => {
+      return results as IGetCustomerDataResponse[]
+    })
+    .catch((err) => {
+      console.error('Failed to fetch customer email', err)
+      return []
+    })
+    .finally(() => {
+      Conn.end()
+    })
+
+  return customerData
+}
--- src/app/api/purchases/mlwebhook/route.ts
+++ src/app/api/purchases/mlwebhook/route.ts
@@ -1,4 +1,6 @@
-import { getPaymentData } from './functions'
+import { sendEmail } from '@/utils/emailFunctions/sendEmail'
+import { getCustomerData, getPaymentData, updatePaymentStatus } from './functions'
+import { AfterPurchaseEmailHTML } from '@/utils/emailFunctions/AfterPurchaseEmail'
 
 interface PaymentUpdate {
   action: string
@@ -13,12 +15,27 @@ interface PaymentUpdate {
   user_id: number
 }
 
-export async function POST(req: Request): Promise<void> {
+export async function POST(req: Request) {
   const body: PaymentUpdate = await req.json()
   console.log('body', body)
 
   const returnValue = await getPaymentData(body.id)
   console.log('returnValue', returnValue)
+
+  updatePaymentStatus({ paymentId: body.id, status: returnValue.status })
+
+  const customerData = await getCustomerData(returnValue.payer.id)
+  console.log('customerData', customerData)
+
+  sendEmail({
+    to: customerData[0].customerEmail,
+    subject: 'Pagamento registrado! Muito obrigado! ^^ ',
+    html: AfterPurchaseEmailHTML({
+      name: customerData[0].customerName.split(' ')[0],
+      order: customerData[0].purchaseIdFK.toString(),
+    }),
+  })
+  return new Response('ok')
 }
 
 export async function GET() {

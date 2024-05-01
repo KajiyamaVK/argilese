12052ceae57dd4136113bb381403e0aa04d056da
victor.kajiyama <victor.kajiyama@newconsoftware.com.br>
2024-04-30 21:16:10 -0300

    fast commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ItemsContainer } from '@/components/ItemsContainer/ItemsContainer'
 import { MainBanner } from '@/components/MainBanner/MainBanner'
 
 export default function Home() {
-  const inMaintenance = false
+  const inMaintenance = true
   if (inMaintenance) {
     return <div className="text-center">Em manutenção</div>
   } else {

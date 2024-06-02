f66b70c6aef5b61427adeab94164b4b37dbef54c
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-02 12:28:53 -0300

    fast commit

--- src/app/api/purchases/mlwebhook/route.ts
+++ src/app/api/purchases/mlwebhook/route.ts
@@ -1,6 +1,6 @@
-import { sendEmail } from '@/utils/emailFunctions/sendEmail'
-import { getCustomerData, getPaymentData, updatePaymentStatus } from './functions'
-import { AfterPurchaseEmailHTML } from '@/utils/emailFunctions/AfterPurchaseEmail'
+// import { sendEmail } from '@/utils/emailFunctions/sendEmail'
+// import { getCustomerData, getPaymentData, updatePaymentStatus } from './functions'
+// import { AfterPurchaseEmailHTML } from '@/utils/emailFunctions/AfterPurchaseEmail'
 
 interface PaymentUpdate {
   action: string
@@ -14,30 +14,30 @@ interface PaymentUpdate {
   type: string
   user_id: number
 }
-
+//teste
 export async function POST(req: Request) {
   const body: PaymentUpdate = await req.json()
   console.log('body', body)
 
-  const returnValue = await getPaymentData(body.data.id)
-  console.log('body.data.id', body.data.id)
-  console.log('returnValue', returnValue)
+  // const returnValue = await getPaymentData(body.data.id)
+  // console.log('body.data.id', body.data.id)
+  // console.log('returnValue', returnValue)
 
-  updatePaymentStatus({ paymentId: body.data.id, status: returnValue.status })
+  // updatePaymentStatus({ paymentId: body.data.id, status: returnValue.status })
 
-  const customerData = await getCustomerData(returnValue.id)
-  console.log('customerData', customerData)
+  // const customerData = await getCustomerData(returnValue.id)
+  // console.log('customerData', customerData)
 
-  if (returnValue.status === 'approved') {
-    sendEmail({
-      to: customerData[0].customerEmail,
-      subject: 'Pagamento registrado! Muito obrigado! ^^ ',
-      html: AfterPurchaseEmailHTML({
-        name: customerData[0].customerName.split(' ')[0],
-        order: customerData[0].purchaseIdFK.toString(),
-      }),
-    })
-  }
+  // if (returnValue.status === 'approved') {
+  //   sendEmail({
+  //     to: customerData[0].customerEmail,
+  //     subject: 'Pagamento registrado! Muito obrigado! ^^ ',
+  //     html: AfterPurchaseEmailHTML({
+  //       name: customerData[0].customerName.split(' ')[0],
+  //       order: customerData[0].purchaseIdFK.toString(),
+  //     }),
+  //   })
+  // }
   return new Response('ok')
 }
 

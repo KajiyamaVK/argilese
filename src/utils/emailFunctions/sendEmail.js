cd526bf7010cc4f8b239f24abe45557efd208cd4
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-07 06:44:50 -0300

    Fix

--- src/utils/emailFunctions/sendEmail.js
+++ src/utils/emailFunctions/sendEmail.js
@@ -15,17 +15,15 @@ export async function sendEmail({ to, html, subject }) {
   console.log('Enviando e-mail para:', to)
   console.log('Título', subject)
   console.log('Corpo', html)
-  await transporter
-    .sendMail({
+  try {
+    const info = await transporter.sendMail({
       from: '"Argile-se" <contato@argilesestudio.com.br>', // remetente
       to: to, // lista de destinatários
       subject: subject, // Assunto
       html: html, // corpo do e-mail em HTML
     })
-    .then((info) => {
-      console.log('E-mail enviado com sucesso', info)
-    })
-    .catch((error) => {
-      console.log('Erro ao enviar e-mail', error)
-    })
+    console.log('E-mail enviado com sucesso', info)
+  } catch (error) {
+    console.log('Erro ao enviar e-mail', error)
+  }
 }

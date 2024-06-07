e6cf5c2442e0cbdfad1f0bd32a9c1234a7b3f7d2
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-07 06:29:45 -0300

    Fix

--- src/utils/emailFunctions/sendEmail.js
+++ src/utils/emailFunctions/sendEmail.js
@@ -12,11 +12,20 @@ const transporter = nodemailer.createTransport({
 })
 
 export async function sendEmail({ to, html, subject }) {
-  console.log('transporter', JSON.stringify(transporter))
-  await transporter.sendMail({
-    from: '"Argile-se" <contato@argilesestudio.com.br>', // remetente
-    to: to, // lista de destinatários
-    subject: subject, // Assunto
-    html: html, // corpo do e-mail em HTML
-  })
+  console.log('Enviando e-mail para:', to)
+  console.log('Título', subject)
+  console.log('Corpo', html)
+  await transporter
+    .sendMail({
+      from: '"Argile-se" <contato@argilesestudio.com.br>', // remetente
+      to: to, // lista de destinatários
+      subject: subject, // Assunto
+      html: html, // corpo do e-mail em HTML
+    })
+    .then((info) => {
+      console.log('E-mail enviado com sucesso', info)
+    })
+    .catch((error) => {
+      console.log('Erro ao enviar e-mail', error)
+    })
 }

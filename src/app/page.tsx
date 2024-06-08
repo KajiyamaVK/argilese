e73615da578638a4c01545cad35d0678f8f6aef6
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-08 14:01:46 -0300

    fast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import { ItemsContainer } from '@/components/ItemsContainer/ItemsContainer'
 import { MainBanner } from '@/components/MainBanner/MainBanner'
 
 export default function Home() {
-  const inMaintenance = false
+  const inMaintenance = true
+  console.log('inMaintenance', inMaintenance, typeof inMaintenance)
+
   if (inMaintenance) {
     return <div className="text-center">Em manutenção</div>
   } else {

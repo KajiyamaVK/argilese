bccd93316efb91cbb3a5e15c82c08ebb3923f20c
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-08 14:08:35 -0300

    fast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ItemsContainer } from '@/components/ItemsContainer/ItemsContainer'
 import { MainBanner } from '@/components/MainBanner/MainBanner'
 
 export default function Home() {
-  const inMaintenance = true
+  const inMaintenance = false
   console.log('inMaintenance', inMaintenance, typeof inMaintenance)
 
   if (inMaintenance) {

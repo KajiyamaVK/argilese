b7795f8cb1a7e4f0183340bf113214e5b43b8509
Victor Kajiyama <65309067+KajiyamaVK@users.noreply.github.com>
2024-06-08 06:55:07 -0300

    Fix:Pix

--- src/components/PaymentBrick/PaymentBrick.tsx
+++ src/components/PaymentBrick/PaymentBrick.tsx
@@ -125,7 +125,6 @@ export function PaymentBrick({ amount, setPaymentId, purchaseId, setPaymentMetho
     const body = {
       ...formData,
       additional_info,
-      notification_url: 'https://argilesestudio.com.br/api/purchases/mlwebhook',
     }
 
     return new Promise<void>(async (resolve, reject) => {
